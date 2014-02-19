01826c5c2f83f5abcc2b87e5c8ae4d71615f5989
Rishabh Tayal <rtayal11@gmail.com>
2014-02-19 16:23:40 -0600

    commit

--- AppikonSite/HTML/contact_config.php
+++ AppikonSite/HTML/contact_config.php
@@ -1,4 +1,4 @@
 <?php
 // To
-define("WEBMASTER_EMAIL", 'rtayal11@gmail.com,rishabh.tayal@appikon.com,info@appikon.com');
+define("WEBMASTER_EMAIL", 'rtayal11@gmail.com,rishabh.tayal@appikon.com,info@appikon.com,shobhit.kasliwal@appikon.com');
 ?>
