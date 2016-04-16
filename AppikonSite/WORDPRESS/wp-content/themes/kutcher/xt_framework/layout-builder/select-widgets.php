fae6a71621f9215a198141ca80ff7c1333b219d6
Rishabh Tayal <rtayal11@gmail.com>
2016-04-16 17:06:26 +0530

    removed wordpress site

--- AppikonSite/WORDPRESS/wp-content/themes/kutcher/xt_framework/layout-builder/select-widgets.php
+++
@@ -1,8 +0,0 @@
-			<select class="widget_selector">
-				<option value="rich_text">Rich Text</option>
-				<option value="divider_line">Divider Normal</option>
-				<option value="divider_dotted">Divider Dotted</option>
-				<option value="divider_shadow">Divider Shadow</option>
-				<option value="divider_vertical_line">Divider Vertical Line</option>
-				<option value="divider_empty">Empty Space</option>
-			</select>
