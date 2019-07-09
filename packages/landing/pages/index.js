8a8135c8278cd9f584324031b3908d566437c37a
Rishabh Tayal <rtayal11@gmail.com>
2019-07-09 10:04:07 -0500

    imports

--- packages/landing/pages/index.js
+++ packages/landing/pages/index.js
@@ -1,27 +1,27 @@
-import React, { Fragment } from 'react';
-import Head from 'next/head';
-import Sticky from 'react-stickynode';
-import { ThemeProvider } from 'styled-components';
-import { agencyTheme } from 'common/src/theme/agency';
 import { ResetCSS } from 'common/src/assets/css/style';
-import {
-  GlobalStyle,
-  AgencyWrapper
-} from 'common/src/containers/Agency/agency.style';
-import Navbar from 'common/src/containers/Agency/Navbar';
-import BannerSection from 'common/src/containers/Agency/BannerSection';
-import FeatureSection from 'common/src/containers/Agency/FeatureSection';
 import AboutUsSection from 'common/src/containers/Agency/AboutUsSection';
-import WorkHistory from 'common/src/containers/Agency/WorkHistory';
+import {
+  AgencyWrapper,
+  GlobalStyle
+} from 'common/src/containers/Agency/agency.style';
+import BannerSection from 'common/src/containers/Agency/BannerSection';
 import BlogSection from 'common/src/containers/Agency/BlogSection';
-import TestimonialSection from 'common/src/containers/Agency/TestimonialSection';
-import TeamSection from 'common/src/containers/Agency/TeamSection';
-import VideoSection from 'common/src/containers/Agency/VideoSection';
+import FaqSection from 'common/src/containers/Agency/FaqSection';
+import FeatureSection from 'common/src/containers/Agency/FeatureSection';
+import Footer from 'common/src/containers/Agency/Footer';
+import Navbar from 'common/src/containers/Agency/Navbar';
 import NewsletterSection from 'common/src/containers/Agency/NewsletterSection';
 import QualitySection from 'common/src/containers/Agency/QualitySection';
-import Footer from 'common/src/containers/Agency/Footer';
+import TeamSection from 'common/src/containers/Agency/TeamSection';
+import TestimonialSection from 'common/src/containers/Agency/TestimonialSection';
+import VideoSection from 'common/src/containers/Agency/VideoSection';
+import WorkHistory from 'common/src/containers/Agency/WorkHistory';
 import { DrawerProvider } from 'common/src/contexts/DrawerContext';
-import FaqSection from 'common/src/containers/Agency/FaqSection';
+import { agencyTheme } from 'common/src/theme/agency';
+import Head from 'next/head';
+import React, { Fragment } from 'react';
+import Sticky from 'react-stickynode';
+import { ThemeProvider } from 'styled-components';
 
 export default () => {
   return (
